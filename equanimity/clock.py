fbf7b70f2fa10770a6df6d4dbce375b0c55275c1
Steve Leonard <sleonard76@gmail.com>
2013-08-24 21:22:08 -0400

    flake8 clock.py

--- equanimity/clock.py
+++ equanimity/clock.py
@@ -4,15 +4,15 @@ clock.py
 Created by AFD on 2013-08-05.
 Copyright (c) 2013 A. Frederick Dudley. All rights reserved.
 """
-import sys
-from datetime import datetime, timedelta
-import json
+from datetime import datetime
 import copy
 
 from zeo import Zeo
 
+
 def now():
     return datetime.utcnow()
+
 
 class Clock():
     """World time related functions."""
@@ -25,20 +25,15 @@ class Clock():
             num = self.uot_num['month'] % 3
         elif uot == 'season':
             num = self.uot_num['season'] % 4
-            if num == 0: return "Earth"
-            elif num == 1: return "Fire"
-            elif num == 2: return "Ice"
-            elif num == 3: return "Wind"
+            season_nums = {0: 'Earth', 1: 'Fire', 2: 'Ice', 3: 'Wind'}
+            return season_nums.get(num)
         else:
             return
 
-        if num == 0: return "One"
-        elif num == 1: return "Two"
-        elif num == 2: return "Three"
-        elif num == 3: return "Four"
-        elif num == 4: return "Five"
-        elif num == 5: return "Six"
-    
+        named_nums = {0: 'One', 1: 'Two', 2: 'Three', 3: 'Four', 4: 'Five',
+                      5: 'Six'}
+        return named_nums.get(num)
+
     def update(self):
         since_dob = (now() - self.DOB).total_seconds()
         dur = self.duration
@@ -50,7 +45,7 @@ class Clock():
             uot_name[uot] = self.get_name(uot)
         self.uot_num = uot_num
         self.uot_name = uot_name
-        
+
     def __init__(self, addr=('localhost', 9100)):
         world = Zeo(addr)
         self.DOB = copy.deepcopy(world.root['DOB'])
@@ -60,15 +55,18 @@ class Clock():
         self.duration['season'] = self.duration['month'] * 3
         self.duration['year'] = self.duration['season'] * 4
         self.duration['generation'] = self.duration['year'] * 14
-        self.uot_num = {'day': 0, 'week': 0, 'month': 0, 'season': 0, 'year': 0, 'generation': 0}
-        self.uot_name = {'day': 'one', 'week': 'one', 'month': 'one', 'season': 'Earth', 'year': 'one', 'generation': 'one'}
+        self.uot_num = {'day': 0, 'week': 0, 'month': 0, 'season': 0,
+                        'year': 0, 'generation': 0}
+        self.uot_name = {'day': 'one', 'week': 'one', 'month': 'one',
+                         'season': 'Earth', 'year': 'one', 'generation': 'one'}
         #Get uot_num and uot_name correct.
         self.update()
         world.db.close()
-        
+
     def since_dob(self, uot=None):
-        """Returns total seconds since DOB in game Units of Time. or seconds."""
-        if uot == None:
+        """Returns total seconds since DOB in game Units of Time.
+        or seconds."""
+        if uot is None:
             return (now() - self.DOB).total_seconds()
         else:
             self.update()
@@ -77,18 +75,17 @@ class Clock():
     def get_delta(self, timedelta, uot):
         """takes a timedelta and returns delta in game UoT."""
         return timedelta.total_seconds() / self.duration[uot]
-        
+
     def get_time(self, uot=None):
         self.update()
         output = ""
         if not uot:
-            output += "Day: %s" %self.uot_name['day'] + "\n"
-            output += "Week: %s" %self.uot_name['week'] + "\n"
-            output += "Month: %s" %self.uot_name['month'] + "\n"
-            output += "Season: %s" %self.uot_name['season'] + "\n"
-            output += "Year: %s" %self.uot_name['year'] + "\n"
-            output += "Generation: %s" %self.uot_name['generation'] + "\n"
+            output += "Day: %s" % self.uot_name['day'] + "\n"
+            output += "Week: %s" % self.uot_name['week'] + "\n"
+            output += "Month: %s" % self.uot_name['month'] + "\n"
+            output += "Season: %s" % self.uot_name['season'] + "\n"
+            output += "Year: %s" % self.uot_name['year'] + "\n"
+            output += "Generation: %s" % self.uot_name['generation'] + "\n"
         else:
             return self.uot_name[uot]
         return output
-            
