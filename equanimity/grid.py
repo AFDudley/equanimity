5df813f2834baf0b85a89e17c6bfc7963abefbad
Steve Leonard <sleonard76@gmail.com>
2013-08-24 21:19:05 -0400

    flake8 grid.py

--- equanimity/grid.py
+++ equanimity/grid.py
@@ -4,17 +4,20 @@ grid.py
 Created by AFD on 2013-08-05.
 Copyright (c) 2013 A. Frederick Dudley. All rights reserved.
 """
-from collections import namedtuple
 import random
-from math import ceil, sqrt
+from collections import namedtuple
 
 from stone import Stone
 
+
 class Loc(namedtuple('Loc', 'x y')):
     __slots__ = ()
+
     def __repr__(self):
-        return '(%r, %r)' %self
+        return '(%r, %r)' % self
+
 noloc = Loc(None, None)
+
 
 class Tile(Stone):
     """Tiles contain units or stones and are used to make battlefields."""
@@ -22,7 +25,8 @@ class Tile(Stone):
     def __init__(self, comp=Stone(), contents=None):
         Stone.__init__(self, comp)
         self.contents = contents
-        
+
+
 class Grid(Stone):
     """A grid is a collection of tiles."""
     #NOTE: The comp that is provided during init is NOT the comp that is set
@@ -38,12 +42,12 @@ class Grid(Stone):
                     temp_comp[suit] += value
         for suit in temp_comp.keys():
             self.comp[suit] = temp_comp[suit] / (self.x * self.y)
-    
+
     def __init__(self, comp=Stone(), x=16, y=16, tiles=None):
         Stone.__init__(self, comp)
         self.x, self.y = self.size = (x, y)
-        if self.value() == 0:
-            if tiles == None:
+        if not self.value():
+            if tiles is None:
                 self.tiles = {}
                 for i in range(x):
                     row = {}
@@ -56,7 +60,7 @@ class Grid(Stone):
                         for suit, value in tiles[x][y].iteritems():
                             self.comp[suit] += value
                 for suit in self.comp.keys():
-                    self.comp[suit] /= self.x * self.y 
+                    self.comp[suit] /= self.x * self.y
                 self.tiles = tiles
         else:
             '''TODO: check for comp/tiles match. Currently assumes if comp,
@@ -64,8 +68,9 @@ class Grid(Stone):
             #creates a pool of comp points to pull from.
             pool = {}
             for suit, value in self.comp.iteritems():
-                pool[suit] = value * self.x * self.y 
-            #pulls comp points from the pool using basis and skew to determine the range of random
+                pool[suit] = value * self.x * self.y
+            #pulls comp points from the pool using basis and skew to
+            # determine the range of random
             #values used to create tiles. Tiles are then shuffled.
             tiles_l = []
             for i in xrange(x-1):
@@ -76,20 +81,22 @@ class Grid(Stone):
                     for suit, value in pool.iteritems():
                         '''This determines the range of the tile comps.'''
                         #good enough for the time being.
-                        basis = self.comp[suit] # this doesn't work as basis approaches limit.
-                        skew  = 2*random.randint((basis/4),(basis*4))
-                        pull  = random.randint(0, min(self.limit[suit], basis+skew))
-                        nv = max(basis/2, min(pull, self.limit[suit]))
+                        # this doesn't work as basis approaches limit:
+                        basis = self.comp[suit]
+                        skew = 2 * random.randint((basis / 4), (basis * 4))
+                        pull = random.randint(0, min(self.limit[suit],
+                                                     basis+skew))
+                        nv = max(basis / 2, min(pull, self.limit[suit]))
                         #print "first nv: %s, pull: %s" % (nv, pull)
                         pool[suit] -= nv
                         new_tile[suit] = nv
                     row_l.append(new_tile)
                 row = {}
-                random.shuffle(row_l) #shuffles tiles in temp. row.
+                random.shuffle(row_l)  # shuffles tiles in temp. row.
                 tiles_l.append(row_l)
-            #special error correcting row (doesn't really work.)
+            # special error correcting row (doesn't really work.)
             row_e = []
-            for k in xrange(y): 
+            for k in xrange(y):
                 new_tile = Stone()
                 for suit, value in pool.iteritems():
                     if pool[suit] != 0:
@@ -101,7 +108,7 @@ class Grid(Stone):
                     pool[suit] -= nv
                     new_tile[suit] = nv
                 row_e.append(new_tile)
-            #hacks upon hacks pt2
+            # hacks upon hacks pt2
             del row_e[-1]
             half = {}
             for suit, value in row_e[-1].iteritems():
@@ -110,38 +117,48 @@ class Grid(Stone):
             row_e.append(half)
             tiles_l.append(row_e)
             self.tiles = {}
-            random.shuffle(tiles_l) #shuffles temp rows.
+            random.shuffle(tiles_l)  # shuffles temp rows.
             for x in xrange(self.x):
                 row = {}
-                for y in xrange(self.y): #This shuffles the tiles before putting them in the grid.
-                    r_index = random.choice(range(len(tiles_l))) # pick a row
-                    c_index = random.choice(range(len(tiles_l[r_index]))) #pick a tile
-                    row.update({y: Tile(tiles_l[r_index][c_index])}) #place tile in grid
-                    del tiles_l[r_index][c_index] #remove used tile
-                    if len(tiles_l[r_index]) == 0: #remove empty rows from tiles_l
+                for y in xrange(self.y):
+                    # This shuffles the tiles before putting them in the grid.
+                    # pick a row
+                    r_index = random.choice(range(len(tiles_l)))
+                    # pick a tile
+                    c_index = random.choice(range(len(tiles_l[r_index])))
+                    # place tile in grid
+                    row.update({y: Tile(tiles_l[r_index][c_index])})
+                    del tiles_l[r_index][c_index]  # remove used tile
+                    if not len(tiles_l[r_index]):
+                        # remove empty rows from tiles_l
                         del tiles_l[r_index]
                 self.tiles.update({x: row})
             del tiles_l
-            #Determine the actual comp NEEDS REAL SOLUTION TODO
+            # Determine the actual comp NEEDS REAL SOLUTION TODO
             self.calc_comp()
 
     def imbue(self, stone):
         raise Exception("Cannot imbue Grid, use imbue_tile instead.")
-    
+
     def imbue_tile(self, tileLoc, stone):
         """Imbues tile with stone, updates grid.comp."""
         self.tiles[tileLoc[0]][tileLoc[1]].imbue(stone)
         self.calc_comp()
-        
+
     def __iter__(self):
         return iter(self.tiles)
+
     def __contains__(self, value):
         return value in self.tiles
-    def __getitem__(self,key):
+
+    def __getitem__(self, key):
         return self.tiles[key]
-    def __setitem__(self,key,value):
+
+    def __setitem__(self, key, value):
         self.tiles[key] = value
+
     def __len__(self):
         return len(self.tiles)
+
     def __repr__(self):
         return dict.__repr__(self.tiles)
