6827dee15d39987f9a2f3331fb5da3dc27b8be45
Steve Leonard <sleonard76@gmail.com>
2013-09-29 16:40:56 -0400

    cover not in battle condition

--- tests/web/battle.py
+++ tests/web/battle.py
@@ -16,6 +16,13 @@ class BattleTest(RPCTestBase):
         self.defender = self.get_user()
         self.attacker = Player('Atk', 'x@gmail.com', 'xxx')
 
+    def _create_units(self):
+        s = Scient(E, create_comp(earth=2))
+        t = Scient(E, create_comp(earth=1))
+        atksquad = Squad(data=[s])
+        defsquad = Squad(data=[t])
+        return s, t, atksquad, defsquad
+
     def _setup_game(self, atksquad, defsquad):
         self.attacker.squads = [atksquad]
         self.defender.squads = [defsquad]
@@ -26,11 +33,20 @@ class BattleTest(RPCTestBase):
         r = getattr(self.proxy, 'pass')((66, 77), 1)
         self.assertError(r, 'Invalid Field')
 
+    def test_field_not_in_battle_game_over(self):
+        s, t, atksquad, defsquad = self._create_units()
+        self._setup_game(atksquad, defsquad)
+        self.f.game.state['game_over'] = True
+        r = getattr(self.proxy, 'pass')(self.loc, 1)
+        self.assertError(r, 'No game is active for this field')
+
+    def test_field_not_in_battle_not_started(self):
+        self.f.game = None
+        r = getattr(self.proxy, 'pass')(self.loc, 1)
+        self.assertError(r, 'No game is active for this field')
+
     def test_pass(self):
-        s = Scient(E, create_comp(earth=2))
-        t = Scient(E, create_comp(earth=1))
-        atksquad = Squad(data=[s])
-        defsquad = Squad(data=[t])
+        s, t, atksquad, defsquad = self._create_units()
         self._setup_game(atksquad, defsquad)
         self.f.place_scient(s, Hex(1, 0))
         self.f.place_scient(t, Hex(1, 0))
@@ -39,10 +55,7 @@ class BattleTest(RPCTestBase):
         self.assertNoError(r)
 
     def test_move(self):
-        s = Scient(E, create_comp(earth=2))
-        t = Scient(E, create_comp(earth=1))
-        atksquad = Squad(data=[s])
-        defsquad = Squad(data=[t])
+        s, t, atksquad, defsquad = self._create_units()
         self._setup_game(atksquad, defsquad)
         self.f.place_scient(s, Hex(1, 0))
         self.f.place_scient(t, Hex(1, 0))
@@ -52,12 +65,9 @@ class BattleTest(RPCTestBase):
         self.assertNoError(r)
 
     def test_attack(self):
-        s = Scient(E, create_comp(earth=2))
-        t = Scient(E, create_comp(earth=1))
+        s, t, atksquad, defsquad = self._create_units()
         wep = Bow(E, create_comp(earth=0))
         t.equip(wep)
-        atksquad = Squad(data=[s])
-        defsquad = Squad(data=[t])
         self._setup_game(atksquad, defsquad)
         self.f.place_scient(s, Hex(2, 4))
         self.f.place_scient(t, Hex(1, 1))
