cedc0abe0ece2d81b44d384ca7932bf0cfea7c4b
Steve Leonard <xsleonard@gmail.com>
2014-02-20 18:21:02 -0500

    really fix it

--- tools/init_db.py
+++ tools/init_db.py
@@ -9,10 +9,13 @@ from server import create_app
 
 def get_args():
     p = argparse.ArgumentParser()
-    p.add_argument('--reset', help='Reset original world')
-    p.add_argument('-v', '--verbose', help='Verbose output')
-    p.add_argument('--grid-radius', help='Radius of world and field grids')
-    p.add_argument('--square-grid', help='Use a square grid')
+    p.add_argument('--reset', action='store_true', help='Reset original world')
+    p.add_argument('-v', '--verbose', action='store_true',
+                   help='Verbose output')
+    p.add_argument('--grid-radius', type=int,
+                   help='Radius of world and field grids')
+    p.add_argument('--square-grid', action='store_true',
+                   help='Use a square grid')
     return p.parse_args()
 
 
