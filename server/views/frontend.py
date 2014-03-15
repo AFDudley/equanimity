78cf3d1437bf5d289a134e706a80edb1abc85c7b
Steve Leonard <xsleonard@gmail.com>
2014-03-15 00:19:24 -0400

    clone btjs3 into server/templates/ and it will be served

--- server/views/frontend.py
+++ server/views/frontend.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+import os
+from flask import Blueprint, render_template, send_file
 from server import csrf
 from server.decorators import api
 
@@ -8,7 +9,14 @@ frontend = Blueprint('frontend', __name__, url_prefix='')
 
 @frontend.route('/')
 def index():
-    return render_template('frontend/index.html')
+    return render_template('btjs3/client.html')
+
+
+# Serve the js from here until nginx handles the static content
+@frontend.route('/js/<path:path>')
+def static_proxy(path):
+    path = os.path.join('templates/btjs3/js', path)
+    return send_file(path)
 
 
 @csrf.include
