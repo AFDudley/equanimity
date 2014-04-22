1038d86d0c5d8c583c5f7828777fd6e98f93d5e7
A. Frederick Dudley <a.frederick.dudley@gmail.com>
2014-04-22 19:09:52 -0400

    run_wsgi_server assume development environment.

--- run_wsgi_server.sh
+++ run_wsgi_server.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
+uwsgi --http :8080 --virtualenv $VIRTUAL_ENV --module server.wsgi:application
 
-source /home/rdn/venv/bin/activate
-
-uwsgi --http :8080 --virtualenv /home/rdn/venv/ --module server.wsgi:application
