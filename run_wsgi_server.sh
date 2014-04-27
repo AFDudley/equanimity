d722b82f66500a1a16af792e468141ed1f1d315d
A. F. Dudley <a.frederick.dudley@gmail.com>
2014-04-27 04:02:50 +0000

    sigh.

--- run_wsgi_server.sh
+++ run_wsgi_server.sh
@@ -2,5 +2,5 @@
 
 #source /home/rdn/venv/bin/activate
 
-uwsgi --processes 50 --gevent 3 --http :5000 --virtualenv $VIRTUAL_ENV --module server.wsgi:application
+uwsgi --processes 25 --gevent 3 --http :5000 --virtualenv $VIRTUAL_ENV --module server.wsgi:application
 
