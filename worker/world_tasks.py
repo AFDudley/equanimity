5418b240c75ffbbd17f28ff4465e80efcc3b1046
A. F. Dudley <a.frederick.dudley@gmail.com>
2014-05-06 10:46:02 -0400

    tick_tock works, needs to be moved to scheduler, still requires correct celery config.

--- worker/world_tasks.py
+++ worker/world_tasks.py
@@ -1,29 +1,50 @@
+import os
 import sys
+import signal
 from datetime import datetime
 import transaction
 from equanimity.const import CLOCK
 from equanimity.clock import WorldClock
 from server import db, create_app
 from celery import Celery
+from celery.task.control import revoke
 import config
 import gevent
 
 celery = Celery(broker='redis://localhost:6379/0')
 celery.conf.update(
     CELERY_BROKER_URL='redis://localhost:6379/0',
-    CELERY_RESULT_BACKEND='redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND='redis://localhost:6379/0',
+    CELERY_DISABLE_RATE_LIMITS = True,
+    CELERYBEAT_SCHEDULE = {} #  TODO put tick tock on the scheduler
 )
 
 from redis import Redis, ConnectionPool
 r = Redis(connection_pool=ConnectionPool(host='localhost', port=6379, db=1))
 
-@celery.task()
+app_n = None
+
+def sigterm_handler(signum, frame):
+    print >>sys.stderr, "SIGTERM handler.  Shutting Down."
+    os.killpg(0, signal.SIGTERM)
+    sys.exit()
+
+@celery.task(name='world_tasks.start_world', ignore_result=True)
 def start_world(world_id):
     """ Starts the game """
+    global app_n
     print "Starting World..."
-    app = create_app(config='production')
-    with app.test_request_context():
+    if app_n == None:
+        app_n = create_app(config='production')
+    with app_n.test_request_context():
+        # zeo load seems to slow down assignment to world variable
+        db.connection.sync()
         world = db['worlds'].get(world_id)
+        while world == None:
+            db.connection.sync()
+            world = db['worlds'].get(world_id)
+            print "Waiting .25 seconds for ZODB."
+            gevent.sleep(.25)
         if world.clock == None:
             if not world.has_fields:
                 world.create_fields()
@@ -33,7 +54,8 @@ def start_world(world_id):
             world.clock = WorldClock()
             uids = world.players.players.keys()
             world.persist()
-            app.do_teardown_request()
+            app_n.do_teardown_request()
+            db.connection.close() 
             for uid in uids:
                 print 'user.{}.worlds'.format(uid)
                 r.publish('user.{}.worlds'.format(uid), "World {0} persisted.".format(world_id))
@@ -43,17 +65,21 @@ def start_world(world_id):
     print "World Start Task Completed."
     return tick_tock.delay(world_id)
 
-@celery.task()
+@celery.task(name='world_tasks.tick_tock', ignore_result=True)
 def tick_tock(world_id):
     """Takes a world ID and advances the clock of that world"""
+    global app_n
+    os.setsid()
+    signal.signal(signal.SIGTERM, sigterm_handler)
     while True:
-        gevent.sleep(CLOCK['day'].seconds)
+        gevent.sleep(.5)
         print "Tick for World {0} started at: {1}".format(world_id, datetime.utcnow())
-        app = create_app(config='production')
-        with app.test_request_context():
+        with app_n.test_request_context():
+            db.connection.sync()
             world = db['worlds'].get(world_id)
             world.clock.tick(world.fields)
             world.persist()
-            app.do_teardown_request()
+            app_n.do_teardown_request()
+            db.connection.close()
         print "Tick for World {0} ended at: {1}".format(world_id, datetime.utcnow())
 
